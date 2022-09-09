ac3137806b3e1fedb92fd5c387a17e2abdb3b85c
Alexandre-Mattos-Cordeiro <alexandre@imobia.app>
2022-09-09 11:37:13 -0300

    Corrige UI da tela de pix

--- pages/empresas/_id/pix.vue
+++ pages/empresas/_id/pix.vue
@@ -3,16 +3,31 @@
     <v-row>
       <v-col>
         <v-btn
-          :disabled="empresa.tem_pix"
+          :disabled="empresa.tem_pix || !empresa.asaas_key"
           color="MedSpringGreen"
           @click="criarChave"
         >
-          Criar PIX
+          Criar chave PIX
         </v-btn>
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col v-if="empresa.asaas_key" cols="12">
+        <v-alert text border="left" type="warning">
+          <span>
+            Para cadastrar o pix para a empresa, por favor cadastre os seguintes
+            campos:
+          </span>
+          <ul>
+            <li v-if="empresa.asaas_key">
+              O
+              <b>Asaas key</b>
+              da empresa
+            </li>
+          </ul>
+        </v-alert>
+      </v-col>
+      <v-col v-else-if="empresa.tem_pix || !empresa.asaas_key">
         <v-radio-group hide-details>
           <v-simple-table class="card elevation-2">
             <thead>
@@ -101,10 +116,14 @@ export default {
       })
     },
     criarChave() {
-      this.$store.dispatch('empresas/cadastrarPix', {
-        empresa_id: this.empresa.id,
-        data: { apelido: 'ChavePixImobia', type: 'EVP' },
-      })
+      this.$store
+        .dispatch('empresas/cadastrarPix', {
+          empresa_id: this.empresa.id,
+          data: { apelido: 'ChavePixImobia', type: 'EVP' },
+        })
+        .then((response) => {
+          this.empresa.tem_pix = true
+        })
     },
   },
 }
