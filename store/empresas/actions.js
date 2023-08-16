96af930bd031dd5b573445e54502daf8fd236115
vitor <vitor@imobia.app>
2023-08-16 09:01:17 -0300

    alteracao de endpoint

--- store/empresas/actions.js
+++ store/empresas/actions.js
@@ -199,7 +199,7 @@ export default {
     console.log(data)
     return new Promise((resolve, reject) => {
       axios
-        .get(`empresas/${data.empresa_id}/gerar-nova-url-onboarding`)
+        .post(`empresas/${data.empresa_id}/gerar-nova-url-onboarding`)
         .then((res) => {
           console.log(res)
           resolve(res)
